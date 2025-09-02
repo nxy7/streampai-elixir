78c6bd441168a9cb1db3350f4d11067550e3a544
Dawid Danieluk <danielukd@gmail.com>
2025-09-02 21:12:02 +0200

    fix port issue

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -100,7 +100,7 @@ if config_env() == :prod do
   # config :streampai, :dns_cluster_query, env!("DNS_CLUSTER_QUERY")
 
   config :streampai, StreampaiWeb.Endpoint,
-    url: [host: host, port: 8443, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: port],
     secret_key_base: secret_key_base,
     check_origin: [
