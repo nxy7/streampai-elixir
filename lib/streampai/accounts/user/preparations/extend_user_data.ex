569640aa31a1d56edfe5fbf6572e989790cae81b
Dawid Danieluk <danielukd@gmail.com>
2025-09-14 12:25:36 +0200

    feat: make app resilient to wrong session keys

--- lib/streampai/accounts/user/preparations/extend_user_data.ex
+++ lib/streampai/accounts/user/preparations/extend_user_data.ex
@@ -10,13 +10,24 @@ defmodule Streampai.Accounts.User.Preparations.ExtendUserData do
 
   @impl true
   def prepare(query, _opts, _context) do
-    Ash.Query.after_action(query, fn _query, [user] ->
-      extended_user =
-        User
-        |> Ash.Query.for_read(:get_by_id, %{id: user.id}, actor: %{id: user.id})
-        |> Ash.read_one!()
+    Ash.Query.after_action(query, fn _query, users ->
+      case users do
+        [user] ->
+          extended_user =
+            User
+            |> Ash.Query.for_read(:get_by_id, %{id: user.id}, actor: %{id: user.id})
+            |> Ash.read_one!()
 
-      {:ok, [extended_user]}
+          {:ok, [extended_user]}
+
+        [] ->
+          # User not found - return empty list to gracefully handle missing user
+          {:ok, []}
+
+        multiple_users ->
+          # This shouldn't happen for get_by_subject, but handle it gracefully
+          {:ok, multiple_users}
+      end
     end)
   end
 end
---
+++ lib/streampai_web/plugs/safe_load_from_session.ex
@@ -0,0 +1,42 @@
+defmodule StreampaiWeb.Plugs.SafeLoadFromSession do
+  @moduledoc """
+  Safely loads user from session, clearing session if user is not found.
+
+  This is useful for worktree scenarios where a user session from one database
+  is not valid in another database.
+  """
+
+  import Plug.Conn
+
+  require Logger
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    try do
+      StreampaiWeb.AuthPlug.load_from_session(conn, [])
+    rescue
+      error ->
+        # Check if this is an authentication-related error
+        error_string = Exception.message(error)
+
+        if auth_related_error?(error_string) do
+          Logger.info("User session invalid in current worktree database - clearing session")
+
+          conn
+          |> clear_session()
+          |> assign(:current_user, nil)
+        else
+          # Re-raise non-authentication errors
+          reraise error, __STACKTRACE__
+        end
+    end
+  end
+
+  defp auth_related_error?(error_string) do
+    String.contains?(error_string, "ExtendUserData") or
+      String.contains?(error_string, "get_by_subject") or
+      String.contains?(error_string, "subject_to_user") or
+      String.contains?(error_string, "no function clause matching")
+  end
+end
