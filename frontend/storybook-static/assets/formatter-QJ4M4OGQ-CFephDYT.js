7e887f00db429bf0d36081840cbe56baa4ac1299
Dawid Danieluk <danielukd@gmail.com>
2025-12-28 01:12:08 +0100

    WIP

--- frontend/storybook-static/assets/formatter-QJ4M4OGQ-CFephDYT.js
+++
@@ -1,5 +0,0 @@
-import { a2 as e, a1 as i, a4 as m } from "./iframe-BQDcX1su.js";
-import "./preload-helper-PPVm8Dsz.js";
-var t = e(m(), 1),
-	p = (0, t.default)(2)(async (r, a) => (r === !1 ? a : i(a)));
-export { p as formatter };
